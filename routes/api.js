37458cf33f42db3022963e9ab7753448d828d91d
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-05-06 13:11:45 -0600

    feat: respond to /api/convert w/input & unit

--- routes/api.js
+++ routes/api.js
@@ -4,4 +4,10 @@ const { expect } = require('chai'),
   convertHandler = new ConvertHandler()
 
 module.exports = function (app) {
+  app.route('/api/convert').get((req, res) => {
+    const { input } = req.query,
+      inputNum = +input.match(/^\d+/)[0],
+      inputUnit = input.match(/[a-z]+$/i)[0]
+    res.json({ inputNum, inputUnit })
+  })
 }
