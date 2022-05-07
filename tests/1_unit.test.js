91227a19b13d93943ce5eb639d9438e90340d92e
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-05-06 21:19:11 -0600

    feat: 3. fractional test

--- tests/1_unit.test.js
+++ tests/1_unit.test.js
@@ -8,13 +8,15 @@ const chai = require('chai'),
 chai.use(chaiHttp)
 
 suite('Unit Tests', function () {
-  test('can read integer input', done => {
+  const convertPath = '/api/convert'
+
+  test('correctly read integer input', done => {
     const integer = 3,
       unit = 'mi'
 
     chai
       .request(server)
-      .get(`/api/convert?input=${integer}${unit}`)
+      .get(`${convertPath}?input=${integer}${unit}`)
       .end((err, res) => {
         const {
           status,
@@ -29,13 +31,13 @@ suite('Unit Tests', function () {
       })
   })
 
-  test('can read a decimal input', done => {
+  test('correctly read a decimal input', done => {
     const decimal = 4.7,
       unit = 'kg'
 
     chai
       .request(server)
-      .get(`/api/convert?input=${decimal}${unit}`)
+      .get(`${convertPath}?input=${decimal}${unit}`)
       .end((err, res) => {
         const {
           status,
@@ -47,6 +49,27 @@ suite('Unit Tests', function () {
         assert.strictEqual(status, 200)
         assert.strictEqual(inputNum, decimal)
         done()
+      })
+  })
+
+  test('correctly read a fractional input', done => {
+    const fraction = '5/7',
+      unit = 'lbs'
+
+    chai
+      .request(server)
+      .get(`${convertPath}?input=${fraction}${unit}`)
+      .end((err, res) => {
+        const {
+          status,
+          ok,
+          body: { inputNum },
+        } = res
+
+        assert.isTrue(ok)
+        assert.strictEqual(status, 200)
+        // 5/7 = 0.7142857142857143 (in node)
+        assert.strictEqual(inputNum, 0.7142857142857143)
       })
   })
 })
