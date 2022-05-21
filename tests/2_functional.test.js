3ff8b1fc8d22e6b9b255086262102f6d0ea023d5
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-05-21 01:38:07 -0600

    test(Functional Tests suite): Convert 10L: GET /api/convert
    Added first functional tests, that verifies the accuracy of the conversion api unit and amount, to within a hundredth of an equal conversion using Google

--- tests/2_functional.test.js
+++ tests/2_functional.test.js
@@ -5,4 +5,35 @@ const chaiHttp = require('chai-http'),
 
 chai.use(chaiHttp)
 
-suite('Functional Tests', function () {})
+suite('Functional Tests', function () {
+  const CONVERT_PATH = '/api/convert'
+
+  test('1. Convert 10L: GET /api/convert', done => {
+    const num = 10,
+      unit = 'L',
+      input = `${num}${unit}`,
+      expectedNum = 2.64172,
+      within = 0.01,
+      expectedUnit = 'gal'
+
+    chai
+      .request(server)
+      .get(`${CONVERT_PATH}?input=${input}`)
+      .end((err, res) => {
+        const {
+          status,
+          ok,
+          body: { initNum, initUnit, returnNum, returnUnit },
+        } = res
+
+        assert.strictEqual(status, 200)
+        assert.isTrue(ok)
+        assert.strictEqual(initNum, num)
+        assert.strictEqual(initUnit, unit.toLowerCase())
+        assert.approximately(returnNum, expectedNum, within)
+        assert.strictEqual(returnUnit, expectedUnit)
+
+        done()
+      })
+  })
+})
